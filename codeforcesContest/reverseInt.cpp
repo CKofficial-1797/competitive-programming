af4a5e83d9d16a8333e6ae3ae8ed7f02dd090b20
CKofficial-1797 <sanjaygorain1797@gmail.com>
2025-11-12 16:40:34 +0530

    change

--- codeforcesContest/reverseInt.cpp
+++ codeforcesContest/reverseInt.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int testCases;
-    cin >> testCases;
-
-    while (testCases--) {
-        int n;
-        string s, t;
-        cin >> n >> s >> t;
-
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-
-        cout << (s == t ? "YES\n" : "NO\n");
+    int t;
+    if (!(cin >> t)) return 0;
+    while (t--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        
+        if (a == b && b == c && c == d) 
+            cout << "YES\n";
+        else 
+            cout << "NO\n";
     }
-
     return 0;
 }
